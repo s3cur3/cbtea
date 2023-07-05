00a209799ffd0098c1c160e5f6d87b482f1c655a
Tyler Young <tyler@tylerayoung.com>
2023-07-04 20:48:04 -0500

    Configure formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,16 @@
 [
-  line_length: 120,
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 98,
+  heex_line_length: 120,
+  import_deps: [
+    :ecto,
+    :ecto_sql,
+    :phoenix,
+    :phoenix_live_view,
+    :union_typespec
+  ],
+  plugins: [
+    Phoenix.LiveView.HTMLFormatter
+  ],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
   subdirectories: ["priv/*/migrations", "priv/repo/data_migrations"]
 ]
