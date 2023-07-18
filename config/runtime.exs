5921ca5c6d72773a05f98d71277f31586bcb5041
Tyler Young <tyler@tylerayoung.com>
2023-07-18 06:11:49 -0500

    Fix SiteEncrypt

--- config/runtime.exs
+++ config/runtime.exs
@@ -67,39 +67,15 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    https: [
+      compress: true,
+      port: port + 1,
+      cipher_suite: :strong,
+      secure_renegotiate: true,
+      reuse_sessions: true,
+      log_level: :warning
+    ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :cbt, CbtWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your endpoint, ensuring
-  # no data is ever sent via http, always redirecting to https:
-  #
-  #     config :cbt, CbtWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
 
   # ## Configuring the mailer
   #
--- lib/cbt_web/endpoint.ex
+++ lib/cbt_web/endpoint.ex
@@ -2,6 +2,8 @@ defmodule CbtWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :cbt
   use SiteEncrypt.Phoenix
 
+  plug SiteEncrypt.AcmeChallenge, __MODULE__
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
