f4e1bc2aa427a8ccf46aa9fe3caca6e02215a30f
Tyler Young <tyler@tylerayoung.com>
2023-07-23 07:13:06 -0500

    Fix max width of flash notifications on small screens

--- lib/cbt_web/components/core_components.ex
+++ lib/cbt_web/components/core_components.ex
@@ -113,7 +113,7 @@ defmodule CbtWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        "fixed top-2 right-2 w-72 sm:w-96 z-50 rounded-lg p-3 ring-1",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
@@ -692,7 +692,6 @@ defmodule CbtWeb.CoreComponents do
 
       <.header_links
         class="hidden md:flex"
-        current_user={@current_user}
         current_user={@current_user}
         log_out={@log_out}
         register={@register}
